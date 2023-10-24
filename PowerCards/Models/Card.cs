95b302d187d7867edafd2e419bbaad4bc9cca75b
Jay <jayjanaskar@gmail.com>
2023-10-24 14:28:29 +0200

    CardId downgrade

--- PowerCards/Models/Card.cs
+++ PowerCards/Models/Card.cs
@@ -5,11 +5,9 @@ namespace PowerCards.Models
 {
     public class Card
     {
-        [Key, Column(Order = 0)]
         [ForeignKey("Deck")]
         public int DeckID { get; set; }
-        [Key, Column(Order = 1)]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public int CardID { get; set; }
         public string? Question { get; set; }
         public string? Answer { get; set; }
