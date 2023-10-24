a3cfa0e5fba9ed32891368389104e706d9941739
Jay <jayjanaskar@gmail.com>
2023-10-24 02:26:48 +0200

    Seeding (not tested)

---
+++ PowerCards/Controllers/CardsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PowerCards.DAL;
+using PowerCards.Models;
+
+namespace PowerCards.Controllers
+{
+    public class CardsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CardsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cards
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Cards.Include(c => c.Deck);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Cards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cards == null)
+            {
+                return NotFound();
+            }
+
+            var card = await _context.Cards
+                .Include(c => c.Deck)
+                .FirstOrDefaultAsync(m => m.DeckID == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
+
+        // GET: Cards/Create
+        public IActionResult Create()
+        {
+            ViewData["DeckID"] = new SelectList(_context.Decks, "DeckID", "Title");
+            return View();
+        }
+
+        // POST: Cards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DeckID,CardID,Question,Answer,Hint")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(card);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DeckID"] = new SelectList(_context.Decks, "DeckID", "Title", card.DeckID);
+            return View(card);
+        }
+
+        // GET: Cards/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cards == null)
+            {
+                return NotFound();
+            }
+
+            var card = await _context.Cards.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+            ViewData["DeckID"] = new SelectList(_context.Decks, "DeckID", "Title", card.DeckID);
+            return View(card);
+        }
+
+        // POST: Cards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DeckID,CardID,Question,Answer,Hint")] Card card)
+        {
+            if (id != card.DeckID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(card);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CardExists(card.DeckID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DeckID"] = new SelectList(_context.Decks, "DeckID", "Title", card.DeckID);
+            return View(card);
+        }
+
+        // GET: Cards/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cards == null)
+            {
+                return NotFound();
+            }
+
+            var card = await _context.Cards
+                .Include(c => c.Deck)
+                .FirstOrDefaultAsync(m => m.DeckID == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
+
+        // POST: Cards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cards == null)
+            {
+                return Problem("Entity set 'AppDbContext.Cards'  is null.");
+            }
+            var card = await _context.Cards.FindAsync(id);
+            if (card != null)
+            {
+                _context.Cards.Remove(card);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CardExists(int id)
+        {
+          return (_context.Cards?.Any(e => e.DeckID == id)).GetValueOrDefault();
+        }
+    }
+}
