af979679ddc9ce3f8374fb5a61604b1edc626bf0
Jay <jayjanaskar@gmail.com>
2023-10-25 15:25:25 +0200

    Acc Pages

--- PowerCards/Program.cs
+++ PowerCards/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using PowerCards.DAL;
 using System;
+using Microsoft.AspNetCore.Identity;
+using PowerCards.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDbContextConnection' not found.");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -11,6 +14,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration["ConnectionStrings:AppDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -23,12 +32,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
 app.UseAuthorization();
-
 app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
