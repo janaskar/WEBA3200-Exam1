553867a716df33d7eb054f730f396df2a86b8e54
Jay <jayjanaskar@gmail.com>
2023-10-26 20:08:19 +0200

    PB fix

--- PowerCards/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ PowerCards/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -26,11 +26,6 @@ namespace PowerCards.Areas.Identity.Pages.Account.Manage
         public async Task<IActionResult> OnGet()
         {
             var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
             return Page();
         }
     }
