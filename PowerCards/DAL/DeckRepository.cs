bf22eafea2a1738c66931fbbd8f8c7229c1bc1e4
CassMayo <124171977+CassMayo@users.noreply.github.com>
2023-10-28 17:59:45 +0200

    DeckRepositry

--- PowerCards/DAL/DeckRepository.cs
+++ PowerCards/DAL/DeckRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace PowerCards.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using PowerCards.Models;
+
+namespace PowerCards.DAL
 {
-    public class DeckRepository
+    public class DeckRepository : IDeckRepository
     {
+        private readonly AppDbContext _context;
+        public DeckRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Deck>> GetAll()
+        {
+            return await _context.Decks.ToListAsync();
+        }
+        public async Task<Deck?> GetById(int id)
+        {
+            return await _context.Decks.FindAsync(id);
+        }
+        public async Task Create(Deck deck)
+        {
+            _context.Decks.Add(deck);
+            await _context.SaveChangesAsync();
+        }
+        public async Task Edit(Deck deck)
+        {
+            _context.Update(deck);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<bool> Delete(int id)
+        {
+            var deck = await _context.Decks.FindAsync(id);
+            if (deck == null)
+            {
+                return false;
+            }
+            _context.Decks.Remove(deck);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
     }
 }
