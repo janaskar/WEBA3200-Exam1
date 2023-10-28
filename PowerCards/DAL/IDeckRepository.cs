9c799a0a2f0da557759faf06997016d7790e11a7
CassMayo <124171977+CassMayo@users.noreply.github.com>
2023-10-28 18:00:14 +0200

    IDeckRepository

--- PowerCards/DAL/IDeckRepository.cs
+++ PowerCards/DAL/IDeckRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace PowerCards.DAL
+﻿using PowerCards.Models;
+namespace PowerCards.DAL
 {
-    public class IDeckRepository
+    public interface IDeckRepository
     {
+        Task<IEnumerable<Deck>> GetAll();
+        Task<Deck?> GetById(int id);
+        Task Create(Deck deck);
+        Task Edit(Deck deck);
+        Task<bool> Delete(int id);
     }
 }
