12a64a165e28f5a9a3a46e22dc0aa745643afc4f
CassMayo <124171977+CassMayo@users.noreply.github.com>
2023-10-28 22:03:42 +0200

    Added comments to DecksController

--- PowerCards/Controllers/DecksController.cs
+++ PowerCards/Controllers/DecksController.cs
@@ -25,6 +25,7 @@ namespace PowerCards.Controllers
         // GET: Decks Index
         public async Task<IActionResult> Index()
         {
+            // Retrieve all decks from the database 
             var decks = await _deckRepository.GetAll();
             return View(decks);
         }
@@ -34,20 +35,26 @@ namespace PowerCards.Controllers
         // GET: Decks Details. This is not a part of CRUD Thats why it stays here
         public async Task<IActionResult> Details(int? id)
         {
+            // Check if the id is null
             if (id == null)
             {
                 return NotFound();
             }
-
+            // Retrieve the deck to be displayed
             var deck = await _deckRepository.GetById((int)id);
+            // Check if the deck exists
             if (deck == null)
             {
+                // If the deck does not exist, return not found
                 return NotFound();
             }
 
+            // Create a view model to hold the deck and the card
             var viewModel = new DeckViewModel
             {
+                // Set the deck to the deck retrieved from the database
                 Deck = deck,
+                // Create a new card with the deck id
                 Card = new Card() { DeckID = deck.DeckID }
             };
             return View(viewModel);
@@ -66,59 +73,74 @@ namespace PowerCards.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("DeckID,UserName,Title,Description,Subject")] Deck deck)
         {
+            // Check if the model state is valid
             if (ModelState.IsValid)
             {
+                // Create the deck
                 await _deckRepository.Create(deck);
+                // Redirect to the deck index view
                 return RedirectToAction(nameof(Index));
             }
             return View(deck);
         }
 
-
+        // GET: Decks Edit
         [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
+            // Check if the id is null
             if (id == null)
             {
+                // If the id is null, return not found
                 return NotFound();
             }
-
+            // Retrieve the deck to be edited
             var deck = await _deckRepository.GetById((int)id);
+            // Check if the deck exists
             if (deck == null)
             {
+                // If the deck does not exist, return not found
                 return NotFound();
             }
             return View(deck);
         }
 
+        // POST: Decks Edit
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("DeckID,UserName,Title,Description,Subject")] Deck deck)
         {
+            // Check if the id of the deck to be edited matches the id of the deck in the model
             if (id != deck.DeckID)
             {
                 return NotFound();
             }
-
+            // Check if the model state is valid
             if (ModelState.IsValid)
             {
                 try
                 {
+                    // Edit the deck
                     await _deckRepository.Edit(deck);
                 }
+                // Check if the deck exists
                 catch (DbUpdateConcurrencyException)
                 {
+                    // If the deck does not exist, return not found
                     if (!(await _deckRepository.GetById(deck.DeckID) is not null))
                     {
                         return NotFound();
                     }
+                    // If the deck exists, throw the exception
                     else
                     {
                         throw;
                     }
                 }
+                // Redirect to the deck details view
                 return RedirectToAction("Details", "Decks", new { id = deck.DeckID });
             }
+            // If the model state is not valid, return the deck
             return View(deck);
         }
 
@@ -127,16 +149,20 @@ namespace PowerCards.Controllers
         [HttpGet]
         public async Task<IActionResult> Delete(int? id)
         {
+            // Check if the id is null
             if (id == null)
             {
                 return NotFound();
             }
-
+            // Retrieve the deck to be deleted
             var deck = await _deckRepository.GetById((int)id);
+            // Check if the deck exists
             if (deck == null)
             {
+                // If the deck does not exist, return not found 
                 return NotFound();
             }
+            // Return the deck to be deleted
             return View(deck);
         }
 
@@ -144,11 +170,16 @@ namespace PowerCards.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            //Retrieve the deck to be deleted
             bool success = await _deckRepository.Delete(id);
+            // Check if the deck exists
             if (!success)
             {
-                return Problem("Error deleting the deck.");
+                // If the deck does not exist, return not found
+                // Til Jay: Vi kan adde errorhandling der notFound er
+                return NotFound();
             }
+            // Redirect to the deck index view
             return RedirectToAction(nameof(Index));
         }
 
@@ -156,6 +187,7 @@ namespace PowerCards.Controllers
         // Utility method to check if a dard exists in the database
         private async Task<bool> DeckExists(int id)
         {
+            // Check if the deck exists by id
             return (await _deckRepository.GetById(id)) != null;
         }
     }
